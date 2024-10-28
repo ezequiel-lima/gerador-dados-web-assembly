d96cdf64af405d69c2bf6274efa2f9def89868ec
ezequiel-lima <ezequiel.lima.br@gmail.com>
2024-10-28 19:56:05 -0300

    CHORE: adicionado novas formas de gerar dados validos para pessoa

--- GeradorDadosWebAssembly/Services/GeradorPessoaService.cs
+++ GeradorDadosWebAssembly/Services/GeradorPessoaService.cs
@@ -5,6 +5,7 @@ using GeradorDadosWebAssembly.Dtos;
 using GeradorDadosWebAssembly.Extensions;
 using GeradorDadosWebAssembly.Services.Interfaces;
 using GeradorDadosWebAssembly.Utils;
+using SecureIdentity.Password;
 
 namespace GeradorDadosWebAssembly.Services
 {
@@ -30,64 +31,43 @@ namespace GeradorDadosWebAssembly.Services
             var pessoa = new Faker<PessoaDto>("pt_BR")
                .RuleFor(p => p.Nome, f => f.Person.FullName.ToString())
                .RuleFor(p => p.Cpf, f => f.Person.Cpf())
-               .RuleFor(p => p.Rg, f => GerarRg(f)) // TO DO
-               .RuleFor(p => p.DataNascimento, f => f.Date.Past(1, DateTime.Now.AddYears(-18)).ToString("dd/MM/yyyy")) // TO DO
-               .RuleFor(p => p.Genero, f => f.Person.Gender.ToString())
-               .RuleFor(p => p.Signo, f => f.Person.Avatar) // TO DO
+               .RuleFor(p => p.Rg, f => GeradorDadosUtil.GerarRgValido())
+               .RuleFor(p => p.DataNascimento, f => f.Date.Past(50, DateTime.Now.AddYears(-18)).ToString("dd/MM/yyyy"))
+               .RuleFor(p => p.Genero, f => GeradorDadosUtil.TraduzirGenero(f.Person.Gender))
+               .RuleFor(p => p.Signo, f => GeradorDadosUtil.GerarSigno(f))
                .RuleFor(p => p.NomeMae, f => f.Name.FullName(Name.Gender.Female))
                .RuleFor(p => p.NomePai, f => f.Name.FullName(Name.Gender.Male))
                .RuleFor(p => p.Email, (f, p) => GeradorDadosUtil.GerarEmail(f, p.Nome))
-               .RuleFor(p => p.Senha, f => f.Person.UserName) // TO DO
+               .RuleFor(p => p.Senha, f => PasswordGenerator.Generate())
                .RuleFor(p => p.Endereco, f => endereco)
                .RuleFor(p => p.NumeroEndereco, f => f.Address.BuildingNumber())
                .RuleFor(p => p.Telefone, f => GeradorDadosUtil.GerarTelefone(f))
                .RuleFor(p => p.Celular, f => GeradorDadosUtil.GerarCelular(f))
-               .RuleFor(p => p.Altura, f => f.Random.Number(100, 210).ToString()) // TO DO
-               .RuleFor(p => p.Peso, f => f.Random.Number(100, 210).ToString()) // TO DO
-               .RuleFor(p => p.TipoSanguineo, f => GerarTipoSanguineo(f))
+               .RuleFor(p => p.Altura, f => (f.Random.Number(130, 200) / 100.0).ToString("0.00").Replace('.', ','))
+               .RuleFor(p => p.Peso, f => $"{f.Random.Number(50, 120)}kg")
+               .RuleFor(p => p.TipoSanguineo, f => GeradorDadosUtil.GerarTipoSanguineo(f))
                .Generate();
+
+            var enderecoApi = await ObterEnderecoApi();
+            AtribuirEnderecoApi(pessoa, enderecoApi);
 
             return pessoa;
         }
 
-        private static string GerarRg(Faker f)
+        private async Task<EnderecoDto> ObterEnderecoApi()
         {
-            // Gera um número de RG base sem o dígito de controle (8 dígitos)
-            var rgBase = f.Random.Replace("########");
-
-            // Calcula o dígito de controle usando o módulo 11
-            int digitoControle = CalcularDigitoControle(rgBase);
-           
-            return $"{rgBase}-{digitoControle}";
+            var cepObtido = CepsBrasilExtension.GetRandomCep();
+            var cep = long.Parse(cepObtido.Replace("-", ""));
+            var enderecoApi = await _enderecoService.ObterEnderecoPorCep(cep);
+            return enderecoApi;
         }
 
-        private static int CalcularDigitoControle(string rgBase)
+        private void AtribuirEnderecoApi(PessoaDto pessoa, EnderecoDto? enderecoApi)
         {
-            int soma = 0;
-            int peso = 9;
-
-            // Calcula a soma ponderada dos dígitos do RG
-            for (int i = 0; i < rgBase.Length; i++)
-            {
-                soma += (rgBase[i] - '0') * peso;
-                peso--;
-            }
-
-            // Calcula o dígito de controle com base no módulo 11
-            int resto = soma % 11;
-            int digitoControle = 11 - resto;
-
-            // Ajusta o dígito para os casos onde é igual a 10 ou 11
-            if (digitoControle == 10) digitoControle = 0;
-            else if (digitoControle == 11) digitoControle = 1;
-
-            return digitoControle;
-        }
-
-        private static string GerarTipoSanguineo(Faker f)
-        {
-            var tiposSanguineos = new[] { "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-" };
-            return f.PickRandom(tiposSanguineos);
+            pessoa.Endereco.Rua = enderecoApi?.Rua ?? pessoa.Endereco.Rua;
+            pessoa.Endereco.Bairro = enderecoApi?.Bairro ?? pessoa.Endereco.Bairro;
+            pessoa.Endereco.Cidade = enderecoApi?.Cidade ?? pessoa.Endereco.Cidade;
+            pessoa.Endereco.Estado = enderecoApi?.Estado ?? pessoa.Endereco.Estado;
         }
     }
 }
--- GeradorDadosWebAssembly/Utils/GeradorDadosUtil.cs
+++ GeradorDadosWebAssembly/Utils/GeradorDadosUtil.cs
@@ -1,4 +1,5 @@
 ﻿using Bogus;
+using Bogus.DataSets;
 using GeradorDadosWebAssembly.Extensions;
 using System.Text.RegularExpressions;
 
@@ -37,6 +38,50 @@ namespace GeradorDadosWebAssembly.Utils
         {
             string[] result = f.Address.StreetName().Split(" ");
             return $"{result[1]} {result[0]}";
+        }
+
+        public static string TraduzirGenero(Name.Gender genero)
+        {
+            return genero == Name.Gender.Male ? "Masculino" : "Feminino";
+        }
+
+        public static string GerarTipoSanguineo(Faker f)
+        {
+            var tiposSanguineos = new[] { "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-" };
+            return f.PickRandom(tiposSanguineos);
+        }
+
+        public static string GerarSigno(Faker f)
+        {
+            var signos = new[]
+            {
+                "Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem",
+                "Libra", "Escorpião", "Sagitário", "Capricórnio", "Aquário", "Peixes"
+            };
+            return f.PickRandom(signos);
+        }
+
+        public static string GerarRgValido()
+        {
+            var random = new Random();
+            var rgBase = random.Next(10000000, 99999999).ToString(); 
+            int[] pesos = { 2, 3, 4, 5, 6, 7, 8, 9 };
+            int soma = 0;
+
+            for (int i = 0; i < rgBase.Length; i++)
+            {
+                soma += (rgBase[i] - '0') * pesos[i];
+            }
+
+            int resto = soma % 11;
+            int digitoVerificador = 11 - resto;
+
+            if (digitoVerificador == 10 || digitoVerificador == 11)
+            {
+                digitoVerificador = 0;
+            }
+
+            return $"{rgBase.Substring(0, 2)}.{rgBase.Substring(2, 3)}.{rgBase.Substring(5, 3)}-{digitoVerificador}";
         }
     }
 }
